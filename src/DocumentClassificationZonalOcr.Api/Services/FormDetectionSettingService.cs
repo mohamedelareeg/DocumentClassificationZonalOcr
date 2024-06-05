ceb4ce67e7f730b24e90173602a7427ef23fd873
PC <PC@DESKTOP-INHVPJC>
2024-06-05 21:03:08 +0300

    default form Detection Settings

--- src/DocumentClassificationZonalOcr.Api/Services/FormDetectionSettingService.cs
+++ src/DocumentClassificationZonalOcr.Api/Services/FormDetectionSettingService.cs
@@ -4,6 +4,7 @@ using DocumentClassificationZonalOcr.Shared.Dtos;
 using DocumentClassificationZonalOcr.Api.Models;
 using DocumentClassificationZonalOcr.Api.Results;
 using DocumentClassificationZonalOcr.Api.Services.Abstractions;
+using DocumentClassificationZonalOcr.Shared.Enums;
 
 namespace DocumentClassificationZonalOcr.Api.Services
 {
@@ -19,8 +20,38 @@ namespace DocumentClassificationZonalOcr.Api.Services
             var result = await _formDetectionSettingsRepository.GetSettingsAsync();
             if (result.IsFailure)
                 return Result.Failure<FormDetectionSetting>(result.Error);
+            var formDetectionSettings = result.Value;
+            if (result.Value is null)
+            {
+                var defaultSettings = FormDetectionSetting.Create(
+                      OcrEngine.TesseractOcr,
+                      5,
+                      DetectOptions.FullImage,
+                      0,
+                      0,
+                      DetectAlgorithm.SIFT,
+                      false,
+                      false,
+                      false,
+                      false,
+                      false,
+                      false,
+                      false
+                  );
+                if (defaultSettings.IsFailure)
+                    return Result.Failure<FormDetectionSetting>(defaultSettings.Error);
 
-            return Result.Success(result.Value);
+                var addSettingsResult = await _formDetectionSettingsRepository.UpdateSettingsAsync(defaultSettings.Value);
+                if (addSettingsResult.IsFailure)
+                    return Result.Failure<FormDetectionSetting>(addSettingsResult.Error);
+
+                result = await _formDetectionSettingsRepository.GetSettingsAsync();
+                if (result.IsFailure)
+                    return Result.Failure<FormDetectionSetting>(result.Error);
+
+                formDetectionSettings = result.Value;
+            }
+            return Result.Success(formDetectionSettings);
         }
 
         public async Task<Result<bool>> UpdateSettingsAsync(FormDetectionSettingDto settingsDto)
